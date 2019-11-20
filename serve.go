edea455bf2f925856b9e48bdafcbd027fe14d241
tom_in_wsl <tailangsheng@gmail.com>
2019-11-20 22:20:46 +0900

    Refact

--- serve.go
+++ serve.go
@@ -6,31 +6,16 @@ import (
 )
 
 func main() {
-	http.Handle("/", http.FileServer(http.Dir("./static")))
+	// http.Handle("/", http.FileServer(http.Dir("./static")))
 
-	// http.HandleFunc("/chart", func(w http.ResponseWriter, r *http.Request) {
-	// 	http.ServeFile(w, r, "./static/cht.html")
-	// 	http.ServeFile(w, r, "./static/cht.css")
-	// 	http.ServeFile(w, r, "./static/reset.css")
-	// 	http.ServeFile(w, r, "./static/cht.js")
-	// 	http.ServeFile(w, r, "./static/chartjs-plugin-streaming.min.js")
-	// })
-
-	// http.HandleFunc("/index", func(w http.ResponseWriter, req *http.Request) {
-	// 	w.Write([]byte("Hello world"))
-	// })
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static/"))))
+	http.HandleFunc("/chart", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "./static/cht.html")
+	})
 
 	log.Printf("Start HTTP server")
 
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		panic(err)
 	}
-
-	// err := http.ListenAndServe(":8080", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-
-	// }))
-
-	// if err != nil {
-	// 	panic(err)
-	// }
 }
